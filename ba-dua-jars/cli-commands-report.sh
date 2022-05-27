92fa79f49412860cbd55b2b247bb9834ce298aa8
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-05-27 11:30:50 +0200

    ba-dua test #19

--- ba-dua-jars/cli-commands-report.sh
+++ ba-dua-jars/cli-commands-report.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-BADUACLI="../ba-dua-jars/ba-dua-cli-0.6.0-all.jar"
-BADUASER="target/badua.ser"
-CLASSES="target/classes"
-BADUAXML="target/badua.xml"
+BADUACLI="/home/runner/work/bookkeeper/bookkeeper/ba-dua-jars/ba-dua-cli-0.6.0-all.jar"
+BADUASER="/home/runner/work/bookkeeper/bookkeeper/bookkeeper-server/target/badua.ser"
+CLASSES="/home/runner/work/bookkeeper/bookkeeper/bookkeeper-server/target/classes"
+BADUAXML="/home/runner/work/bookkeeper/bookkeeper/bookkeeper-server/target/badua.xml"
 
 java -jar ${BADUACLI} report    \
         -input ${BADUASER}      \
