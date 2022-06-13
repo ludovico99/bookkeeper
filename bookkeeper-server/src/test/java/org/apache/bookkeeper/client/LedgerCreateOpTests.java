73eab9085b5e46449a7cf90930b316c51cfb0678
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-06-13 13:49:42 +0200

    Try all tests execution in LedgerCreateOpTests

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/LedgerCreateOpTests.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/LedgerCreateOpTests.java
@@ -19,97 +19,10 @@ import java.util.concurrent.CompletableFuture;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
-@RunWith(value = Parameterized.class)
-
-
-public class LedgerCreateOpTests extends BookKeeperClusterTestCase {
-
-    private Object expectedException;
-    private Integer ensembleSize;
-    private Integer writeQuorumSize;
-    private Integer ackQuorumSize;
-    private SyncCallbackUtils.SyncCreateCallback cb;
-
-    public LedgerCreateOpTests(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, ParamType cb, Object expectedException) {
-        super(3);
-        configureInitiate(ensembleSize, writeQuorumSize, ackQuorumSize, cb, expectedException);
-    }
-
-    private void configureInitiate(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, ParamType cb, Object expectedException) {
-
-        this.expectedException = expectedException;
-        this.ensembleSize = ensembleSize;
-        this.writeQuorumSize = writeQuorumSize;
-        this.ackQuorumSize = ackQuorumSize;
-
-        switch (cb){
-            case VALID_INSTANCE:
-                this.cb = createCallback();
-                break;
-            case NULL_INSTANCE:
-                this.cb = null;
-                break;
-        }
-
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> getParameters() {
-        return Arrays.asList(new Object[][]{
-                //Totale bookies = 3
-                //ensembleSize,       writeQuorumSize,     ackQuorumSize        CB                                 Exception
-                {2,                           2,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.OK},
-                {3,                           2,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.OK},
-                {3,                           1,                  1,      ParamType.VALID_INSTANCE,             BKException.Code.OK},
-                {1,                           2,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.ZKException},
-                {3,                           4,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.ZKException},
-                {11,                          10,                 2,      ParamType.VALID_INSTANCE,             BKException.Code.NotEnoughBookiesException},
-                {3,                           4,                  5,      ParamType.VALID_INSTANCE,             BKException.Code.ZKException},
-                {3,                           2,                  3,      ParamType.VALID_INSTANCE,             BKException.Code.ZKException},
-                {3,                           4,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.ZKException},
-                {10,                          5,                  2,      ParamType.VALID_INSTANCE,             BKException.Code.NotEnoughBookiesException},
-                {null,                        1,                  1,      ParamType.VALID_INSTANCE,             new NullPointerException()},
-                {3,                           null,               1,      ParamType.VALID_INSTANCE,             new NullPointerException()},
-                {3,                           1,               null,      ParamType.VALID_INSTANCE,             new NullPointerException()}
-
-
-        });
-    }
 
 
 
-    @Test
-    public void Test_Initiate() {
-
-         try {
-             BookKeeper bookkeeper = this.bkc;
-             Counter counter = new Counter();
-             LedgerCreateOp ledgerCreateOp = new LedgerCreateOp(bookkeeper, ensembleSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.CRC32,
-                     "pwd".getBytes(StandardCharsets.UTF_8), this.cb, counter, null, EnumSet.allOf(WriteFlag.class), bookkeeper.getClientCtx().getClientStats());
-
-             if ((int) this.expectedException == BKException.Code.ZKException) {
-                 // Non va i busy waiting poichè mi aspetto di non avere risposta !!!
-                 ledgerCreateOp.initiate();
-                 verifyNoInteractions(this.cb);
-             } else {
-                 counter.inc();
-
-                 ledgerCreateOp.initiate();
-
-                 counter.wait(0);
-
-                 ArgumentCaptor<Integer> argument = ArgumentCaptor.forClass(int.class);
-                 verify(this.cb).createComplete(argument.capture(), nullable(LedgerHandle.class), isA(Object.class));
-                 Assert.assertEquals(this.expectedException, argument.getValue());
-             }
-
-
-         } catch (Exception e) {
-             e.printStackTrace();
-             Assert.assertEquals("Exception that i expect is raised", this.expectedException.getClass(), e.getClass());
-         }
-
-    }
+public class LedgerCreateOpTests {
 
     private static class Counter {
         int i;
@@ -137,28 +50,26 @@ public class LedgerCreateOpTests extends BookKeeperClusterTestCase {
     }
 
     @RunWith(value = Parameterized.class)
-    public static class LedgerCreateOpInitiateAdvTest extends BookKeeperClusterTestCase {
+    public static class LedgerCreateOpInitiateTest extends BookKeeperClusterTestCase {
 
         private Object expectedException;
         private Integer ensembleSize;
         private Integer writeQuorumSize;
         private Integer ackQuorumSize;
-        private Long ledgerId;
         private SyncCallbackUtils.SyncCreateCallback cb;
 
-        public LedgerCreateOpInitiateAdvTest(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, Long ledgerId, ParamType cb, Object expectedException) {
+
+        public LedgerCreateOpInitiateTest(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, ParamType cb, Object expectedException) {
             super(3);
-            configureInitiateAdv(ensembleSize, writeQuorumSize, ackQuorumSize,ledgerId, cb, expectedException);
+            configureInitiate(ensembleSize, writeQuorumSize, ackQuorumSize, cb, expectedException);
         }
 
-
-        private void configureInitiateAdv(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize ,Long ledgerId, ParamType cb, Object expectedException) {
+        private void configureInitiate(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, ParamType cb, Object expectedException) {
 
             this.expectedException = expectedException;
             this.ensembleSize = ensembleSize;
             this.writeQuorumSize = writeQuorumSize;
             this.ackQuorumSize = ackQuorumSize;
-            this.ledgerId = ledgerId;
 
             switch (cb) {
                 case VALID_INSTANCE:
@@ -175,23 +86,20 @@ public class LedgerCreateOpTests extends BookKeeperClusterTestCase {
         public static Collection<Object[]> getParameters() {
             return Arrays.asList(new Object[][]{
                     //Totale bookies = 3
-                    //ensembleSize,       writeQuorumSize,     ackQuorumSize    ledgerId             CB                                 Exception
-                    {2,                   2,                        2,             0L,             ParamType.VALID_INSTANCE,               BKException.Code.OK},
-                    {3,                   2,                        2,             0L,             ParamType.VALID_INSTANCE,               BKException.Code.OK},
-                    {3,                   1,                        1,             0L,             ParamType.VALID_INSTANCE,               BKException.Code.OK},
-                    {1,                   2,                        2,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   4,                        2,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {11,                  10,                       2,             0L,             ParamType.VALID_INSTANCE,               BKException.Code.NotEnoughBookiesException},
-                    {3,                   4,                        5,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   2,                        3,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   4,                        2,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {10,                  5,                        2,             0L,             ParamType.VALID_INSTANCE,               BKException.Code.NotEnoughBookiesException},
-                    {null,                1,                        1,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   null,                     1,             0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   2,                        null,          0L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   2,                        1,            -1L,             ParamType.VALID_INSTANCE,               BKException.Code.OK},
-                    {3,                   2,                        1,            -2L,             ParamType.VALID_INSTANCE,               true},
-                    {3,                   2,                        1,            null,            ParamType.VALID_INSTANCE,               true}
+                    //ensembleSize,       writeQuorumSize,     ackQuorumSize        CB                                 Exception
+                    {2, 2, 2, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                    {3, 2, 2, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                    {3, 1, 1, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                    {1, 2, 2, ParamType.VALID_INSTANCE, BKException.Code.ZKException},
+                    {3, 4, 2, ParamType.VALID_INSTANCE, BKException.Code.ZKException},
+                    {11, 10, 2, ParamType.VALID_INSTANCE, BKException.Code.NotEnoughBookiesException},
+                    {3, 4, 5, ParamType.VALID_INSTANCE, BKException.Code.ZKException},
+                    {3, 2, 3, ParamType.VALID_INSTANCE, BKException.Code.ZKException},
+                    {3, 4, 2, ParamType.VALID_INSTANCE, BKException.Code.ZKException},
+                    {10, 5, 2, ParamType.VALID_INSTANCE, BKException.Code.NotEnoughBookiesException},
+                    {null, 1, 1, ParamType.VALID_INSTANCE, new NullPointerException()},
+                    {3, null, 1, ParamType.VALID_INSTANCE, new NullPointerException()},
+                    {3, 1, null, ParamType.VALID_INSTANCE, new NullPointerException()}
 
 
             });
@@ -199,23 +107,22 @@ public class LedgerCreateOpTests extends BookKeeperClusterTestCase {
 
 
         @Test
-        public void Test_InitiateAdv() {
+        public void Test_Initiate() {
 
             try {
                 BookKeeper bookkeeper = this.bkc;
-                LedgerCreateOpTests.Counter counter = new LedgerCreateOpTests.Counter();
+                Counter counter = new Counter();
                 LedgerCreateOp ledgerCreateOp = new LedgerCreateOp(bookkeeper, ensembleSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.CRC32,
                         "pwd".getBytes(StandardCharsets.UTF_8), this.cb, counter, null, EnumSet.allOf(WriteFlag.class), bookkeeper.getClientCtx().getClientStats());
 
                 if ((int) this.expectedException == BKException.Code.ZKException) {
                     // Non va i busy waiting poichè mi aspetto di non avere risposta !!!
-                    ledgerCreateOp.initiateAdv(this.ledgerId);
+                    ledgerCreateOp.initiate();
                     verifyNoInteractions(this.cb);
-
                 } else {
                     counter.inc();
 
-                    ledgerCreateOp.initiateAdv(this.ledgerId);
+                    ledgerCreateOp.initiate();
 
                     counter.wait(0);
 
@@ -223,27 +130,125 @@ public class LedgerCreateOpTests extends BookKeeperClusterTestCase {
                     verify(this.cb).createComplete(argument.capture(), nullable(LedgerHandle.class), isA(Object.class));
                     Assert.assertEquals(this.expectedException, argument.getValue());
                 }
+
+
             } catch (Exception e) {
                 e.printStackTrace();
-                Assert.assertTrue("Exception that i expect is raised", (boolean) this.expectedException);
-
+                Assert.assertEquals("Exception that i expect is raised", this.expectedException.getClass(), e.getClass());
             }
 
         }
-    }
 
-    private static SyncCallbackUtils.SyncCreateCallback createCallback(){
 
-        return spy(new SyncCallbackUtils.SyncCreateCallback (new CompletableFuture<>()){
+        @RunWith(value = Parameterized.class)
+        public static class LedgerCreateOpInitiateAdvTest extends BookKeeperClusterTestCase {
 
-            @Override
-            public void createComplete(int rc, LedgerHandle lh, Object ctx) {
-                Counter counter = (Counter) ctx;
-                counter.dec();
-                System.out.println("Create complete: rc = " + rc + " counter value: " + counter.i);
+            private Object expectedException;
+            private Integer ensembleSize;
+            private Integer writeQuorumSize;
+            private Integer ackQuorumSize;
+            private Long ledgerId;
+            private SyncCallbackUtils.SyncCreateCallback cb;
+
+            public LedgerCreateOpInitiateAdvTest(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, Long ledgerId, ParamType cb, Object expectedException) {
+                super(3);
+                configureInitiateAdv(ensembleSize, writeQuorumSize, ackQuorumSize, ledgerId, cb, expectedException);
             }
 
-        });
+
+            private void configureInitiateAdv(Integer ensembleSize, Integer writeQuorumSize, Integer ackQuorumSize, Long ledgerId, ParamType cb, Object expectedException) {
+
+                this.expectedException = expectedException;
+                this.ensembleSize = ensembleSize;
+                this.writeQuorumSize = writeQuorumSize;
+                this.ackQuorumSize = ackQuorumSize;
+                this.ledgerId = ledgerId;
+
+                switch (cb) {
+                    case VALID_INSTANCE:
+                        this.cb = createCallback();
+                        break;
+                    case NULL_INSTANCE:
+                        this.cb = null;
+                        break;
+                }
+
+            }
+
+            @Parameterized.Parameters
+            public static Collection<Object[]> getParameters() {
+                return Arrays.asList(new Object[][]{
+                        //Totale bookies = 3
+                        //ensembleSize,       writeQuorumSize,     ackQuorumSize    ledgerId             CB                                 Exception
+                        {2, 2, 2, 0L, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                        {3, 2, 2, 0L, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                        {3, 1, 1, 0L, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                        {1, 2, 2, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, 4, 2, 0L, ParamType.VALID_INSTANCE, true},
+                        {11, 10, 2, 0L, ParamType.VALID_INSTANCE, BKException.Code.NotEnoughBookiesException},
+                        {3, 4, 5, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, 2, 3, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, 4, 2, 0L, ParamType.VALID_INSTANCE, true},
+                        {10, 5, 2, 0L, ParamType.VALID_INSTANCE, BKException.Code.NotEnoughBookiesException},
+                        {null, 1, 1, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, null, 1, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, 2, null, 0L, ParamType.VALID_INSTANCE, true},
+                        {3, 2, 1, -1L, ParamType.VALID_INSTANCE, BKException.Code.OK},
+                        {3, 2, 1, -2L, ParamType.VALID_INSTANCE, true},
+                        {3, 2, 1, null, ParamType.VALID_INSTANCE, true}
+
+
+                });
+            }
+
+
+            @Test
+            public void Test_InitiateAdv() {
+
+                try {
+                    BookKeeper bookkeeper = this.bkc;
+                    LedgerCreateOpTests.Counter counter = new LedgerCreateOpTests.Counter();
+                    LedgerCreateOp ledgerCreateOp = new LedgerCreateOp(bookkeeper, ensembleSize, writeQuorumSize, ackQuorumSize, BookKeeper.DigestType.CRC32,
+                            "pwd".getBytes(StandardCharsets.UTF_8), this.cb, counter, null, EnumSet.allOf(WriteFlag.class), bookkeeper.getClientCtx().getClientStats());
+
+                    if ((int) this.expectedException == BKException.Code.ZKException) {
+                        // Non va i busy waiting poichè mi aspetto di non avere risposta !!!
+                        ledgerCreateOp.initiateAdv(this.ledgerId);
+                        verifyNoInteractions(this.cb);
+
+                    } else {
+                        counter.inc();
+
+                        ledgerCreateOp.initiateAdv(this.ledgerId);
+
+                        counter.wait(0);
+
+                        ArgumentCaptor<Integer> argument = ArgumentCaptor.forClass(int.class);
+                        verify(this.cb).createComplete(argument.capture(), nullable(LedgerHandle.class), isA(Object.class));
+                        Assert.assertEquals(this.expectedException, argument.getValue());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Assert.assertTrue("Exception that i expect is raised", (boolean) this.expectedException);
+
+                }
+
+            }
+        }
+
+        private static SyncCallbackUtils.SyncCreateCallback createCallback() {
+
+            return spy(new SyncCallbackUtils.SyncCreateCallback(new CompletableFuture<>()) {
+
+                @Override
+                public void createComplete(int rc, LedgerHandle lh, Object ctx) {
+                    Counter counter = (Counter) ctx;
+                    counter.dec();
+                    System.out.println("Create complete: rc = " + rc + " counter value: " + counter.i);
+                }
+
+            });
+        }
     }
 }
 
