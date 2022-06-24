aa9481531db53e30725b489e922746aee8307161
Ludovico Zarrelli <ludovico.zarr@gmail.com>
2022-06-24 13:31:59 +0200

    Pit report

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/BookieClientImplGetListsOfEntriesLedgerTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/BookieClientImplGetListsOfEntriesLedgerTest.java
@@ -25,7 +25,6 @@ import java.util.*;
 import java.util.concurrent.Executors;
 
 
-@Ignore
 @RunWith(value = Parameterized.class)
 public class BookieClientImplGetListsOfEntriesLedgerTest extends BookKeeperClusterTestCase {
 
